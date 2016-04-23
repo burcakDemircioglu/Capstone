fed5da21ef32952d70f702052e44a530796db6f8
burcak <burcakdemircioglu@gmail.com>
2016-04-23 20:31:37 +0300

    feat: Change Typeface of the Title

--- WannaBeer/app/src/main/java/com/example/burcakdemircioglu/wannabeer/ui/MainActivity.java
+++ WannaBeer/app/src/main/java/com/example/burcakdemircioglu/wannabeer/ui/MainActivity.java
@@ -12,6 +12,7 @@ import android.content.Loader;
 import android.database.Cursor;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.widget.SwipeRefreshLayout;
@@ -20,6 +21,8 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -37,7 +40,7 @@ public class MainActivity extends AppCompatActivity implements
     private SwipeRefreshLayout mSwipeRefreshLayout;
     private RecyclerView mRecyclerView;
     private Toolbar mToolbar;
-
+    private CollapsingToolbarLayout mCollapsingToolbarLayout;
     static final String EXTRA_STARTING_ALBUM_POSITION = "extra_starting_item_position";
     static final String EXTRA_CURRENT_ALBUM_POSITION = "extra_current_item_position";
 
@@ -85,7 +88,7 @@ public class MainActivity extends AppCompatActivity implements
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        //mToolbar = (Toolbar) findViewById(R.id.toolbar);
         //setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -98,6 +101,13 @@ public class MainActivity extends AppCompatActivity implements
         });
 
         mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh_layout);
+        mCollapsingToolbarLayout=(CollapsingToolbarLayout)findViewById(R.id.collapsing_toolbar_layout);
+        mCollapsingToolbarLayout.setTitle("WannaBeer");
+        mCollapsingToolbarLayout.setCollapsedTitleTypeface(Typeface.createFromAsset(getResources().getAssets(), "erasdust.ttf"));
+        mCollapsingToolbarLayout.setExpandedTitleTypeface(Typeface.createFromAsset(getResources().getAssets(), "erasdust.ttf"));
+
+        mCollapsingToolbarLayout.setCollapsedTitleTextColor(getResources().getColor(R.color.image_background));
+        mCollapsingToolbarLayout.setExpandedTitleColor(getResources().getColor(R.color.image_background));
 
         mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         getLoaderManager().initLoader(0, null, this);
@@ -252,11 +262,11 @@ public class MainActivity extends AppCompatActivity implements
             subtitleView = (TextView) view.findViewById(R.id.article_subtitle);
         }
     }
-    /*
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        //getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
@@ -272,8 +282,12 @@ public class MainActivity extends AppCompatActivity implements
             return true;
         }
 
+        if (id == R.id.action_menu) {
+            return true;
+        }
+
         return super.onOptionsItemSelected(item);
     }
-    */
+
 
 }
