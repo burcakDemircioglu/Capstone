83c105f04ced12be1ca0831bb73b8a42a97c5f95
burcak <burcakdemircioglu@gmail.com>
2016-04-27 01:53:23 +0300

    feat: Create Favourites Page's Template Without the Panes and ListViews

--- WannaBeer/app/src/main/java/com/example/burcakdemircioglu/wannabeer/ui/FavoritesActivity.java
+++ WannaBeer/app/src/main/java/com/example/burcakdemircioglu/wannabeer/ui/FavoritesActivity.java
@@ -1,32 +1,48 @@
 package com.example.burcakdemircioglu.wannabeer.ui;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.Window;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import com.example.burcakdemircioglu.wannabeer.R;
 
 public class FavoritesActivity extends AppCompatActivity {
-
+    ImageButton mUpButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorites);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+        Window window=getWindow();
+        window.setStatusBarColor(getResources().getColor(R.color.colorPrimaryDark));
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        ImageView profileForeground = (ImageView) findViewById(R.id.profile_foreground);
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.profile_foreground);
+
+        RoundedBitmapDrawable drawable = RoundedBitmapDrawableFactory.create(getResources(), bitmap);
+        drawable.setCircular(true);
+        profileForeground.setImageDrawable(drawable);
+
+        mUpButton = (ImageButton) findViewById(R.id.action_up);
+        mUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                onSupportNavigateUp();
+                supportFinishAfterTransition();
             }
         });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 
 }
