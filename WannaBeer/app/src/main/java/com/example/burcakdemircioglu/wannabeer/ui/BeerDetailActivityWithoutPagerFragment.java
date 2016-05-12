7b0bbe3d05f5e0c773d494de0cbdf2fd14b0ec3b
burcak <burcakdemircioglu@gmail.com>
2016-05-13 00:42:43 +0300

    feat: Add Like Dislike Buttons to Detail Pages Without Pager

--- WannaBeer/app/src/main/java/com/example/burcakdemircioglu/wannabeer/ui/BeerDetailActivityWithoutPagerFragment.java
+++ WannaBeer/app/src/main/java/com/example/burcakdemircioglu/wannabeer/ui/BeerDetailActivityWithoutPagerFragment.java
@@ -1,5 +1,10 @@
 package com.example.burcakdemircioglu.wannabeer.ui;
 
+import android.content.ContentProviderOperation;
+import android.content.ContentValues;
+import android.content.OperationApplicationException;
+import android.net.Uri;
+import android.os.RemoteException;
 import android.support.v4.app.LoaderManager;
 import android.content.Intent;
 import android.support.v4.content.Loader;
@@ -18,24 +23,37 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.ImageLoader;
 import com.example.burcakdemircioglu.wannabeer.R;
+import com.example.burcakdemircioglu.wannabeer.data.BeersContract;
+import com.example.burcakdemircioglu.wannabeer.data.DislikedBeersLoader;
 import com.example.burcakdemircioglu.wannabeer.data.InfoLoader;
+import com.example.burcakdemircioglu.wannabeer.data.LikedBeersLoader;
 import com.example.burcakdemircioglu.wannabeer.ui.util.DrawInsetsFrameLayout;
 import com.example.burcakdemircioglu.wannabeer.ui.util.ImageLoaderHelper;
 import com.example.burcakdemircioglu.wannabeer.ui.util.ObservableScrollView;
+
+import java.util.ArrayList;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class BeerDetailActivityWithoutPagerFragment extends Fragment implements
         LoaderManager.LoaderCallbacks<Cursor>{
+    private static final String TAG = "BeerDetailFragment";
+
     private Cursor mCursor;
+    private Cursor mCursorLiked;
+    private Cursor mCursorDisliked;
+
     private long mItemId;
+    private String mItemName;
     private View mRootView;
     private int mTopInset;
     private View mPhotoContainerView;
@@ -44,6 +62,14 @@ public class BeerDetailActivityWithoutPagerFragment extends Fragment implements
 
     private ImageView mFlagView;
     private ImageView mPhotoView;
+    private ImageButton mLikeButton;
+    private ImageButton mDislikeButton;
+
+    private int likeDislikeInteraction;
+    private static final int LIKE=0;
+    private static final int DISLIKE=1;
+    private static final int NO_INTERACTION =2;
+
     private int mMutedColor = 0xFF333333;
     private ObservableScrollView mScrollView;
     private DrawInsetsFrameLayout mDrawInsetsFrameLayout;
@@ -113,6 +139,9 @@ public class BeerDetailActivityWithoutPagerFragment extends Fragment implements
        // mPhotoView.setTransitionName(Integer.toString(mPosition));
         //Log.e("onClickTransNameDetail", Integer.toString(mPosition));
 
+        mLikeButton= (ImageButton)mRootView.findViewById(R.id.like_button);
+        mDislikeButton=(ImageButton)mRootView.findViewById(R.id.dislike_button);
+
         mPhotoContainerView = mRootView.findViewById(R.id.photo_container);
 
         mStatusBarColorDrawable = new ColorDrawable(0);
@@ -162,7 +191,111 @@ public class BeerDetailActivityWithoutPagerFragment extends Fragment implements
             bodyView.setText(mCursor.getString(InfoLoader.Query.DESCRIPTION));
             String location= "Location: "+mCursor.getString(InfoLoader.Query.LOCATION);
             locationView.setText(location);
+            if (likeDislikeInteraction==LIKE){
+                mLikeButton.setImageDrawable(getResources().getDrawable(R.drawable.like_button_filter));
+                mDislikeButton.setImageDrawable(getResources().getDrawable(R.drawable.dislike_button));
 
+            }
+            else if(likeDislikeInteraction==DISLIKE){
+                mLikeButton.setImageDrawable(getResources().getDrawable(R.drawable.like_button));
+                mDislikeButton.setImageDrawable(getResources().getDrawable(R.drawable.dislike_button_filter));
+            }
+            else{
+                mLikeButton.setImageDrawable(getResources().getDrawable(R.drawable.like_button));
+                mDislikeButton.setImageDrawable(getResources().getDrawable(R.drawable.dislike_button));
+            }
+
+            mLikeButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ArrayList<ContentProviderOperation> cpo = new ArrayList<ContentProviderOperation>();
+                    Uri dirUri = BeersContract.LikedItems.buildDirUri();
+                    ContentValues values = new ContentValues();
+                    values.put(BeersContract.LikedItems.BEER_NAME, mCursor.getString(InfoLoader.Query.NAME));
+
+                    if (likeDislikeInteraction==NO_INTERACTION) {
+                        likeDislikeInteraction=LIKE;
+                        cpo.add(ContentProviderOperation.newInsert(dirUri).withValues(values).build());
+                        Log.e("LikeDislikeButtons",mCursor.getString(InfoLoader.Query.NAME)+" added to Like Table");
+                    }
+                    else if (likeDislikeInteraction==DISLIKE) {
+                        likeDislikeInteraction=LIKE;
+                        cpo.add(ContentProviderOperation.newInsert(dirUri).withValues(values).build());
+                        Log.e("LikeDislikeButtons",mCursor.getString(InfoLoader.Query.NAME)+" added to Like Table");
+
+                        Uri dirUriDislike = BeersContract.DislikedItems.buildDirUri();
+
+                        String[] args = new String[] {mCursor.getString(InfoLoader.Query.NAME)};
+                        cpo.add(ContentProviderOperation.newDelete(dirUriDislike).withSelection(BeersContract.DislikedItems.BEER_NAME + "=?", args).build());
+                        Log.e("LikeDislikeButtons",mCursor.getString(InfoLoader.Query.NAME)+"deleted from Dislike Table");
+                    }
+                    else {
+                        likeDislikeInteraction=NO_INTERACTION;
+                        String[] args = new String[] {mCursor.getString(InfoLoader.Query.NAME)};
+                        cpo.add(ContentProviderOperation.newDelete(dirUri).withSelection(BeersContract.LikedItems.BEER_NAME + "=?", args).build());
+                        Log.e("LikeDislikeButtons",mCursor.getString(InfoLoader.Query.NAME)+" deleted from Like Table");
+
+
+                    }
+                    try {
+                        getActivity().getContentResolver().applyBatch(BeersContract.CONTENT_AUTHORITY, cpo);
+
+                    }catch (RemoteException | OperationApplicationException e) {
+                        Log.e(TAG, "Error updating content.", e);
+                    }
+
+                    bindViews();
+                    Toast.makeText(getActivity(), "Added to lov'd List", Toast.LENGTH_SHORT).show();
+
+
+                }
+            });
+            mDislikeButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ArrayList<ContentProviderOperation> cpo = new ArrayList<ContentProviderOperation>();
+                    Uri dirUri = BeersContract.DislikedItems.buildDirUri();
+                    ContentValues values = new ContentValues();
+                    values.put(BeersContract.DislikedItems.BEER_NAME, mCursor.getString(InfoLoader.Query.NAME));
+
+                    if(likeDislikeInteraction==NO_INTERACTION){
+                        likeDislikeInteraction=DISLIKE;
+                        cpo.add(ContentProviderOperation.newInsert(dirUri).withValues(values).build());
+                        Log.e("LikeDislikeButtons",mCursor.getString(InfoLoader.Query.NAME)+"added to Dislike Table");
+                        Toast.makeText(getActivity(), "Added to meh.. List", Toast.LENGTH_SHORT).show();
+
+                    }
+                    else if(likeDislikeInteraction==LIKE){
+                        likeDislikeInteraction=DISLIKE;
+                        cpo.add(ContentProviderOperation.newInsert(dirUri).withValues(values).build());
+                        Log.e("LikeDislikeButtons",mCursor.getString(InfoLoader.Query.NAME)+"added to Dislike Table");
+                        Toast.makeText(getActivity(), "Added to meh.. List", Toast.LENGTH_SHORT).show();
+
+                        Uri dirUriLike = BeersContract.LikedItems.buildDirUri();
+
+                        String[] args = new String[] {mCursor.getString(InfoLoader.Query.NAME)};
+                        cpo.add(ContentProviderOperation.newDelete(dirUriLike).withSelection(BeersContract.LikedItems.BEER_NAME + "=?", args).build());
+                        Log.e("LikeDislikeButtons",mCursor.getString(InfoLoader.Query.NAME)+" deleted from Like Table");
+
+                    }
+                    else {
+                        likeDislikeInteraction=NO_INTERACTION;
+                        String[] args = new String[] {mCursor.getString(InfoLoader.Query.NAME)};
+                        cpo.add(ContentProviderOperation.newDelete(dirUri).withSelection(BeersContract.DislikedItems.BEER_NAME + "=?", args).build());
+                        Log.e("LikeDislikeButtons",mCursor.getString(InfoLoader.Query.NAME)+"deleted from Dislike Table");
+
+                    }
+
+                    try {
+                        getActivity().getContentResolver().applyBatch(BeersContract.CONTENT_AUTHORITY, cpo);
+
+                    } catch (RemoteException | OperationApplicationException e) {
+                        Log.e(TAG, "Error updating content.", e);
+                    }
+
+                    bindViews();
+                }
+            });
             ImageLoaderHelper.getInstance(getActivity()).getImageLoader()
                     .get(mCursor.getString(InfoLoader.Query.BOTTLE), new ImageLoader.ImageListener() {
                         @Override
@@ -210,7 +343,12 @@ public class BeerDetailActivityWithoutPagerFragment extends Fragment implements
     }
     @Override
     public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
-        return InfoLoader.newInstanceForItemId(getActivity(), mItemId);
+        if (i==0)
+            return InfoLoader.newInstanceForItemId(getActivity(), mItemId);
+        else if(i==1)
+            return LikedBeersLoader.newInstanceForItemName(getActivity(), mItemName);
+        else
+            return DislikedBeersLoader.newInstanceForItemName(getActivity(),mItemName);
     }
 
     @Override
@@ -222,11 +360,39 @@ public class BeerDetailActivityWithoutPagerFragment extends Fragment implements
             return;
         }
 
-        mCursor = cursor;
-        if (mCursor != null && !mCursor.moveToFirst()) {
-            Log.e("beerDetailWithoutPager", "Error reading item detail cursor");
-            mCursor.close();
-            mCursor = null;
+        if(cursorLoader.getId()==0) {
+            mCursor = cursor;
+            if (mCursor != null && !mCursor.moveToFirst()) {
+                Log.e(TAG, "Error reading item detail cursor");
+                mCursor.close();
+                mCursor = null;
+            }
+            if(mCursor!=null) {
+                mItemName = mCursor.getString(InfoLoader.Query.NAME);
+                Log.e("onLoadFinished",mItemName);
+                getLoaderManager().initLoader(1, null, this);
+                getLoaderManager().initLoader(2, null, this);
+            }
+        }
+        if(cursorLoader.getId()==1) {
+            mCursorLiked = cursor;
+            if (mCursorLiked != null && !mCursorLiked.moveToFirst()) {
+                Log.e(TAG, "Error reading item detail cursor");
+                mCursorLiked.close();
+                mCursorLiked = null;
+            }
+            if(mCursorLiked!=null)
+                likeDislikeInteraction=LIKE;
+        }
+        if(cursorLoader.getId()==2) {
+            mCursorDisliked = cursor;
+            if (mCursorDisliked != null && !mCursorDisliked.moveToFirst()) {
+                Log.e(TAG, "Error reading item detail cursor");
+                mCursorDisliked.close();
+                mCursorDisliked = null;
+            }
+            if(mCursorDisliked!=null)
+                likeDislikeInteraction=DISLIKE;
         }
 
         bindViews();
